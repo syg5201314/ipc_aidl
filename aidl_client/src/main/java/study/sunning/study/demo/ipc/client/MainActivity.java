fe24524ac5d80f3c2d5970621fc805f9c7eeda92
Sunning <shanyg@live.com>
2017-07-17 00:21:51 +0800

    finish demo

--- aidl_client/src/main/java/study/sunning/study/demo/ipc/client/MainActivity.java
+++ aidl_client/src/main/java/study/sunning/study/demo/ipc/client/MainActivity.java
@@ -1,13 +1,56 @@
 package study.sunning.study.demo.ipc.client;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    IAddService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
+        intent.setAction("android.sunning.demo.ipc.aidl");
+        intent.setPackage("sunning.study.ipc.binder");
+        bindService(intent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                service = IAddService.Stub.asInterface(iBinder);
+                Log.i("Client","Bind Success:" + service);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+
+            }
+        }, Context.BIND_AUTO_CREATE);
+        findViewById(R.id.click_service).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (service != null) {
+                    try {
+                        int result = service.add(100, 200);
+                        Log.e("result","result:" + result);
+                        ((TextView) findViewById(R.id.result)).setText("result:" + result);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
+
+
 }
--- app/src/main/java/sunning/study/ipc/binder/MainActivity.java
+++ app/src/main/java/sunning/study/ipc/binder/MainActivity.java
@@ -1,7 +1,10 @@
 package sunning.study.ipc.binder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sunning.study.ipc.binder.service.AddService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent service = new Intent(this, AddService.class);
+        startService(service);
     }
 }
